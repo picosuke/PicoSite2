1091976ec704dcb409ddeea0761ff4b963f307e9
picosuke <picosuke.rice@gmail.com>
2025-11-14 12:58:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -387,7 +387,7 @@ auth.onAuthStateChanged(user => {
 
     document.getElementById("main2_c").style.display = "block";
     document.getElementById("main2_t").style.display = "none";
-    document.getElementById("you_name").style.display = "block";
+    document.getElementById("you_name").style.display = "flex";
     let initial = (user.email?.trim()?.charAt(0) || "").toUpperCase();
     document.getElementById("you_name").textContent = initial;
     document.getElementById("you_name").style.backgroundColor = nameToColor(user.email);
